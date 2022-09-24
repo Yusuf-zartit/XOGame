efa31cdbfa3dae39b1009668638fc42c71c64ae9
yusufzartit93@gmail.com <yusufzartit93@gmail.com>
2022-09-24 16:26:44 +0300

    add (html, css) to Start component

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -3,7 +3,25 @@ import React from 'react';
 const Start = () => {
     return (
         <div className={"start"}>
-            start
+            <div className={"start__header"}>
+                x o
+            </div>
+            <div className={"card shadow-gray"}>
+                <h1 className={"text-lg"}>Pick player 1's mark</h1>
+                <div className={"start__players"}>
+                    <span className={"start__players--active"}>
+                        x
+                    </span>
+                    <span>
+                        o
+                    </span>
+                </div>
+                <p className={"text-light"}> remember: x goes first</p>
+            </div>
+            <div className={"start__btns"}>
+                <button className={"btn btn-yellow"}>new game (vs CPU)</button>
+                <button className={"btn btn-blue"}>new game (vs Player)</button>
+            </div>
         </div>
     );
 };
